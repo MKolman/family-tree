ffa76e186b6fb15d583eab537ccbcb68348f9adf
Maks Kolman <maks@kolman.si>
2022-01-03 23:04:37 +0100

    Updates

---
+++ precache-manifest.fe3ceefad52098f0c2ce11ce3f030e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7dcb9632f53537f66a1",
+    "url": "/css/app.2f56c452.css"
+  },
+  {
+    "revision": "06bb66c7616b0b011c180ef60bbb1687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7dcb9632f53537f66a1",
+    "url": "/js/app.3cd16b04.js"
+  },
+  {
+    "revision": "9d88b4dcf706e52a748c",
+    "url": "/js/chunk-vendors.c9450d21.js"
+  },
+  {
+    "revision": "b87d20b304859496ee1547b6a3af90d4",
+    "url": "/kolman.pb.aes"
+  },
+  {
+    "revision": "3b088bb637c0f2aefa044bb186d02108",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
